8ef0a4a15ca77a786e10f6a173eb63c28275a6ce
wdggat <wdggat@163.com>
2013-11-21 01:08:30 +0800

    add dependencies in pom

--- src/main/java/com/liu/servlets/Servlet.java
+++ src/main/java/com/liu/servlets/Servlet.java
@@ -1,5 +1,30 @@
 package com.liu.servlets;
 
-public class Servlet {
+import java.io.IOException;
 
+import javax.servlet.ServletConfig;
+import javax.servlet.ServletException;
+import javax.servlet.http.HttpServlet;
+import javax.servlet.http.HttpServletRequest;
+import javax.servlet.http.HttpServletResponse;
+
+import org.apache.log4j.Logger;
+
+public class LogServlet extends HttpServlet {
+	private static final Logger log = Logger.getLogger(LogServlet.class);
+	
+	@Override
+	public void init(ServletConfig config) throws ServletException {
+		super.init(config);
+	}
+	
+	@Override
+	public void doGet(HttpServletRequest request, HttpServletResponse response) throws ServletException, IOException {
+		doPost(request, response);
+	}
+	
+	@Override
+	public void doPost(HttpServletRequest request, HttpServletResponse response) throws ServletException, IOException{
+		
+	}
 }
