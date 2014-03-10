3b7c46e2f171f033e69157f08cb95461849afd78
刘小龙 <hzliuxiaolong@corp.netease.com>
2014-03-10 20:46:02 +0800

    init

--- src/main/java/com/liu/repost/MailSender.java
+++ src/main/java/com/liu/repost/MailSender.java
@@ -12,18 +12,18 @@ import javax.activation.DataHandler;
 import javax.activation.DataSource;
 import javax.activation.FileDataSource;
 import javax.mail.BodyPart;
+import javax.mail.Message;
 import javax.mail.MessagingException;
 import javax.mail.Multipart;
 import javax.mail.Session;
-import javax.mail.Message;
 import javax.mail.Transport;
 import javax.mail.internet.AddressException;
 import javax.mail.internet.InternetAddress;
 import javax.mail.internet.MimeBodyPart;
 import javax.mail.internet.MimeMessage;
 import javax.mail.internet.MimeMultipart;
-import javax.mail.internet.MimeUtility;
 import javax.mail.PasswordAuthentication;
+import javax.mail.internet.MimeUtility;
 
 import org.apache.log4j.Logger;
 
